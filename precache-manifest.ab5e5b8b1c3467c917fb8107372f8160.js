d1f316ffdf38b5af25a1870c09e0e4a8733047e5
NhonNguyen <vi.nhon.53th@gamil.com>
2022-06-14 22:18:58 +0700

    Updates

---
+++ precache-manifest.ab5e5b8b1c3467c917fb8107372f8160.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69082a4f194402143f4896b563a23604",
+    "url": "/mycv/index.html"
+  },
+  {
+    "revision": "fcb77afea658721a93e9",
+    "url": "/mycv/static/css/main.d2c9f889.chunk.css"
+  },
+  {
+    "revision": "e74b99e1d0ac1628573b",
+    "url": "/mycv/static/js/2.3d1cc125.chunk.js"
+  },
+  {
+    "revision": "fcb77afea658721a93e9",
+    "url": "/mycv/static/js/main.e343f5a5.chunk.js"
+  },
+  {
+    "revision": "416e2a1773091db81577",
+    "url": "/mycv/static/js/runtime-main.1928fad4.js"
+  },
+  {
+    "revision": "de79f14165a819a856eea71955feba1f",
+    "url": "/mycv/static/media/avatar.de79f141.jpg"
+  }
+]);
